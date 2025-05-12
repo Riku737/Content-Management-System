87614b43578b2f3326eb11d3da625cd892c51c39
Riki <rmcal024@uottawa.ca>
2025-05-12 11:07:07 -0400

    Established work area

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!-- Root of the website -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Images Library</title>
+</head>
+<body>
+    <h1>Website Coming Soon</h1>
+</body>
+</html>
---
+++ public/staff/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
+        <title>Images Source</title>
+    </head>
+    <body>
+        
+        <header>
+            <h1>Staff Area</h1>
+        </header>
+
+        <navigation>
+            <ul>
+                <li><a href="index.php">Menu</a></li>
+            </ul>
+        </navigation>
+
+        <footer>
+            &copy; <?php echo date('Y'); ?> 2025 Website
+        </footer>
+
+    </body>
+</html>
